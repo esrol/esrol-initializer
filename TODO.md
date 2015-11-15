26aa5f7213f7e2e6e16d429d4631f471d9d8d251
Ivaylo Ivanov <ivchoyyyy@gmail.com>
2015-11-15 09:15:05 +0200

    init

---
+++ TODO.md
@@ -0,0 +1,8 @@
+#To do
+- [ ] Set comments
+- [ ] Build
+- [ ] Tests
+- [ ] Examples
+- [ ] Coverage
+- [ ] Update readme
+- [ ] Update version
---
+++ tests/test.js
@@ -0,0 +1,13 @@
+'use strict';
+let expect = require('chai').expect;
+let Initializer = require('../index.js');
+let initializer = new Initializer();
+let mocks = require ('./mocks/index');
+
+describe('Initializer Success...', () => {
+  describe('When registerComponent', () => {
+    it('Should return true on success', () => {
+
+    });
+  });
+});
