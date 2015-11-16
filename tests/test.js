eff12cd591b54175d75a57fda44cbe561c9c5de0
Ivaylo Ivanov <ivchoyyyy@gmail.com>
2015-11-16 21:59:06 +0200

    working prototype

--- tests/test.js
+++ tests/test.js
@@ -3,11 +3,12 @@ let expect = require('chai').expect;
 let Initializer = require('../index.js');
 let initializer = new Initializer();
 let mocks = require ('./mocks/index');
+require('mocha-sinon');
 
 describe('Initializer Success...', () => {
-  describe('When registerComponent', () => {
-    it('Should return true on success', () => {
 
-    });
-  });
+});
+
+describe('Initializer Success...', () => {
+
 });
